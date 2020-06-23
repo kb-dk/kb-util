0fa39b408177e8353d3f46c9b52325e3fb8b6344
Toke Eskildsen <toes@kb.dk>
2020-06-23 15:01:26 +0200

    Add unit test for nested structures

--- src/test/java/dk/kb/util/YAMLUtilsTest.java
+++ src/test/java/dk/kb/util/YAMLUtilsTest.java
@@ -3,7 +3,9 @@ package dk.kb.util;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,4 +19,35 @@ class YAMLUtilsTest {
         assertEquals("Hello World", x.getProperty("test.somestring"));
         assertEquals("{test.arrayofstrings=[a, b, c], test.somestring=Hello World, test.someint=87, test.nested.sublevel2string=sub1}",x.toString());
     }
+
+    @Test
+    public void testNestedMaps() throws IOException {
+        YAML yaml = YAML.resolveConfig("nested_maps.yml", "test");
+
+        String flattenedValues = YAMLUtils.toFlatList(yaml, null, true).
+                map(Map.Entry::getValue).map(Object::toString).
+                collect(Collectors.joining(", "));
+        assertEquals("barA1, barA2, barB", flattenedValues);
+    }
+
+    @Test
+    public void testNestedLists() throws IOException {
+        YAML yaml = YAML.resolveConfig("nested_lists.yml", "test");
+
+        String flattenedValues = YAMLUtils.toFlatList(yaml, null, true).
+                map(Map.Entry::getValue).map(Object::toString).
+                collect(Collectors.joining(", "));
+        assertEquals("llItemA1, llItemA2, llItemB", flattenedValues);
+    }
+
+    @Test
+    public void testNestedMix() throws IOException {
+        YAML yaml = YAML.resolveConfig("nested_mix.yml", "test");
+
+        String flattenedValues = YAMLUtils.toFlatList(yaml, null, true).
+                map(Map.Entry::getValue).map(Object::toString).
+                collect(Collectors.joining(", "));
+        assertEquals("barM, 87", flattenedValues);
+    }
+
 }
