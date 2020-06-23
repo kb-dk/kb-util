ee053fed51ac67db604cbef1bc91e7dd4b88ebd8
Asger Askov Blekinge <abr@kb.dk>
2020-06-23 12:34:14 +0200

    Review followup + flattenList

--- src/main/java/dk/kb/util/YAMLUtils.java
+++ src/main/java/dk/kb/util/YAMLUtils.java
@@ -1,9 +1,10 @@
 package dk.kb.util;
 
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class YAMLUtils {
@@ -15,47 +16,54 @@ public class YAMLUtils {
      * @return the yaml as properties
      */
     public static Properties toProperties(YAML collection) {
-        Map<String, Object> propertiesMap = toFlatList(collection, null)
-                                                    .collect(Collectors.toMap(
-                                                            tuple2 -> tuple2._1,
-                                                            tuple2 -> tuple2._2
-                                                    ));
-        
         Properties properties = new Properties();
-        
-        properties.putAll(propertiesMap);
+        toFlatList(collection, null, false)
+                .forEach(tuple -> properties.put(tuple.getKey(), tuple.getValue()));
         return properties;
     }
     
-    protected static Stream<Tuple2<String, Object>> toFlatList(Map<String, Object> collection, String keyFix) {
-    
-        return collection.entrySet().stream().flatMap(
-                entry -> {
-                    String key = Stream.of(keyFix,entry.getKey()).filter(Objects::nonNull).collect(Collectors.joining("."));
-                    Object value = entry.getValue();
-                    if (value instanceof Map) {
-                        return toFlatList((Map<String, Object>) value, key);
-                    } else {
-                        return Stream.of(new Tuple2<>(key, value));
-                    }
-                });
+    /**
+     * Converts the YAML object into a properties object
+     *
+     * @param collection the yaml
+     * @param flattenList should lists be flattened like test.arrayofstrings.0=a or kept like test.arrayofstrings=[a, b, c]
+     * @return the yaml as properties
+     *
+     */
+    public static Properties toProperties(YAML collection, boolean flattenList) {
+        Properties properties = new Properties();
+        toFlatList(collection, null, flattenList)
+                .forEach(tuple -> properties.put(tuple.getKey(), tuple.getValue()));
+        return properties;
     }
     
-    
-    public static class Tuple2<K,V> {
+    @SuppressWarnings("unchecked")
+    protected static Stream<Map.Entry<String, Object>> toFlatList(final Map<String, Object> collection,
+                                                                  final String keyFix,
+                                                                  final boolean flattenList) {
         
-        K _1;
-        V _2;
-        
-        public Tuple2(K _1, V _2) {
-            this._1 = _1;
-            this._2 = _2;
-        }
-        
-        @Override
-        public String toString() {
-            return _1 + "=" + _2 + "";
-        }
+        return collection.entrySet().stream().flatMap(
+                entry -> {
+                    //Prefix the key with the preceding keys
+                    String key = keyFix == null ? entry.getKey() : keyFix + "." + entry.getKey();
+                    
+                    //handle the value
+                    Object value = entry.getValue();
+                    if (value instanceof Map) {
+                        //If this is a map, go down, and send the key as the new prefix.
+                        return toFlatList((Map<String, Object>) value, key, flattenList);
+                    } if (flattenList && value instanceof List) {
+                        List<Object> valueList = (List<Object>) value;
+                        ArrayList<Map.Entry<String,Object>> result = new ArrayList<>();
+                        for (int i = 0; i < valueList.size(); i++) {
+                            result.add(new AbstractMap.SimpleEntry<>(key + "." + i, valueList.get(i)));
+                        }
+                        return result.stream();
+                    } else {
+                        //Otherwise just return this tuple
+                        return Stream.of(new AbstractMap.SimpleEntry<>(key, value));
+                    }
+                });
     }
 }
 
