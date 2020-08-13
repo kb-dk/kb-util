41e7a0f5ba53327dbbd83d1b9cdec2ad04655ca5
Asger Askov Blekinge <abr@kb.dk>
2020-08-13 14:57:34 +0200

    NamedThread documented and tested

--- src/main/java/dk/kb/util/other/NamedThread.java
+++ src/main/java/dk/kb/util/other/NamedThread.java
@@ -4,6 +4,27 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * Autoclosable thread namer. To be used like
+ * <code>
+ *     try (NamedThread named = new NamedThread("threadname")) {
+ *
+ *     }
+ * </code>
+ *
+ * <code>
+ *     try (NamedThread named = NamedThread.as("threadname")) {
+ *
+ *     }
+ * </code>
+ *
+ * It will rename the current thread while inside the try-block and restore it's name
+ * upon closing.
+ *
+ * Most useful if you log the thread name as part of your log lines.
+ *
+ * Setting the thread name to some value means you do not have to log this value as part of every log statement
+ */
 public class NamedThread implements AutoCloseable {
     
     
@@ -11,15 +32,25 @@ public class NamedThread implements AutoCloseable {
     private final String oldName;
     private final String name;
     
+    /**
+     * Name the thread from the line number is was created on
+     */
     public NamedThread() {
-        StackTraceElement caller = Thread.currentThread().getStackTrace()[2];
+        //[0] is right here
+        //[1] is this method
+        //[2] is the caller
+        Thread currentThread = Thread.currentThread();
+        StackTraceElement caller = currentThread.getStackTrace()[2];
         this.name = caller.getClassName() + "(" + caller.getFileName() + ":" + caller.getLineNumber() + ") ";
-        currentThread = Thread.currentThread();
-        oldName = currentThread.getName();
-        currentThread.setName(name);
+        this.currentThread = currentThread;
+        oldName = this.currentThread.getName();
+        this.currentThread.setName(name);
     }
     
-    
+    /**
+     * Name the thread
+     * @param name the name to set
+     */
     public NamedThread(String name) {
         this.name = name;
         currentThread = Thread.currentThread();
@@ -27,6 +58,21 @@ public class NamedThread implements AutoCloseable {
         currentThread.setName(name);
     }
     
+    /**
+     * Set the name of the thread
+     * @param name the new thread name
+     * @return a NamedThread
+     * @see #NamedThread(String)
+     */
+    public static NamedThread as(String name) {
+        return new NamedThread(name);
+    }
+    
+    /**
+     * Append the name to the thread name, separated by ->
+     * @param name the postfix to the threadname
+     * @return a NamedThread
+     */
     public static NamedThread postfix(String name) {
         String parentName = Thread.currentThread().getName();
         String threadName = parentName + "->" + name;
@@ -51,27 +97,61 @@ public class NamedThread implements AutoCloseable {
         return oldName;
     }
     
-    
-    public static <T, R> Function<T, R> namedThread(Function<T, R> function, Function<T, String> name) {
+    /**
+     * Wrapper to a function to execute it in a named thread.
+     *
+     * To be used like
+     *
+     * <code>
+     *     .map(NamedThread.namedThread(a -> doSomethingAdvanced(a),
+     *                                 a -> a.toString()))
+     * </code>
+     *
+     * Each invocation of doSomethingAdvanced will run with a thread named
+     * from the input value a. This is especially relevant when working with parallel streams.
+     *
+     * @param function the function to wrap
+     * @param namingFunction The function to compute a name for the invocation
+     * @param <T> The input type to the function
+     * @param <R> the return type of the function
+     * @return a function that will name the thread before each invocation
+     */
+    public static <T, R> Function<T, R> namedThread(Function<T, R> function, Function<T, String> namingFunction) {
         return t -> {
-            try (NamedThread namedThread = new NamedThread(name.apply(t))) {
+            try (NamedThread namedThread = new NamedThread(namingFunction.apply(t))) {
                 return function.apply(t);
             }
         };
     }
     
-    public static <T> Consumer<T> namedThread(Consumer<T> consumer, Function<T, String> name) {
+    /**
+     * Same as #namedThread(Function, Function) but for a Consumer
+     * @see #namedThread(Function, Function)
+     * @param consumer the consumer to wrap
+     * @param namingFunction The function to compute a name for the invocation
+     * @param <T> the input type of the consumer
+     * @return a wrapped consumer
+     */
+    public static <T> Consumer<T> namedThread(Consumer<T> consumer, Function<T, String> namingFunction) {
         return t -> {
-            try (NamedThread namedThread = new NamedThread(name.apply(t))) {
+            try (NamedThread namedThread = new NamedThread(namingFunction.apply(t))) {
                 consumer.accept(t);
             }
         };
         
     }
     
-    public static <T> Predicate<T> namedThread(Predicate<T> predicate, Function<T, String> name) {
+    /**
+     * Same as #namedThread(Function, Function) but for a Predicate
+     * @see #namedThread(Function, Function)
+     * @param predicate the predicate to wrap
+     * @param namingFunction The function to compute a name for the invocation
+     * @param <T> the input type of the predicate
+     * @return a wrapped predicate
+     */
+    public static <T> Predicate<T> namedThread(Predicate<T> predicate, Function<T, String> namingFunction) {
         return t -> {
-            try (NamedThread namedThread = new NamedThread(name.apply(t))) {
+            try (NamedThread namedThread = new NamedThread(namingFunction.apply(t))) {
                 return predicate.test(t);
             }
         };
---
+++ src/test/java/dk/kb/util/other/NamedThreadTest.java
@@ -0,0 +1,106 @@
+package dk.kb.util.other;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NamedThreadTest {
+    
+    @Test
+    void as() {
+        
+        String oldName = Thread.currentThread().getName();
+        assertThat(oldName, is(not("testName")));
+        
+        try (NamedThread named = NamedThread.as("testName")) {
+            assertThat(Thread.currentThread().getName(), is(("testName")));
+        }
+        assertThat(Thread.currentThread().getName(), is(oldName));
+        
+    }
+    
+    @Test
+    void postfix() {
+        String oldName = Thread.currentThread().getName();
+        assertThat(oldName, is(not("testName")));
+        
+        try (NamedThread named = NamedThread.postfix("testName")) {
+            assertThat(Thread.currentThread().getName(), is((oldName + "->testName")));
+        }
+        assertThat(Thread.currentThread().getName(), is(oldName));
+        
+    }
+    
+    
+    @Test
+    void namedThreadFunction() {
+        String oldName = Thread.currentThread().getName();
+        assertThat(oldName, is(not("testName")));
+        
+        IntStream.range(0, 10).mapToObj(i -> i + "")
+                 .parallel()
+                 .map(NamedThread.namedThread((String i) -> Thread.currentThread().getName() + i,
+                                              i -> "testName"))
+                 .forEach(element -> assertThat(element, startsWith("testName")));
+    
+        IntStream.range(0, 10).mapToObj(i -> i + "")
+                 .parallel()
+                 .map((String i) -> Thread.currentThread().getName() + i)
+                 .forEach(element -> assertThat(element, not(startsWith("testName"))));
+        
+    }
+    
+    @Test
+    void testNamedThreadConsumer() {
+        String oldName = Thread.currentThread().getName();
+        assertThat(oldName, is(not("testName")));
+        
+        IntStream.range(0, 10).mapToObj(i -> i + "")
+                 .parallel()
+                 .forEach(NamedThread.namedThread((String i) -> {
+                                                      String threadname = Thread.currentThread().getName();
+                                                      assertThat(threadname, startsWith("testName"));
+                                                  },
+                                                  i -> "testName"));
+    
+        IntStream.range(0, 10).mapToObj(i -> i + "")
+                 .parallel()
+                 .forEach((String i) -> {
+                                                      String threadname = Thread.currentThread().getName();
+                                                      assertThat(threadname, not(startsWith("testName")));
+                                                  });
+        
+    }
+    
+    @Test
+    void testNamedThreadPredicate() {
+    
+        String oldName = Thread.currentThread().getName();
+        assertThat(oldName, is(not("testName")));
+    
+        long renamed = IntStream.range(0, 10).mapToObj(i -> i + "")
+                                .parallel()
+                                .filter(NamedThread.namedThread((String i) -> Thread.currentThread()
+                                                                                    .getName()
+                                                                                    .startsWith("testName"),
+                                                            i -> "testName"))
+                                .count();
+        assertThat(renamed,is(10));
+        long notRenamed = IntStream.range(0, 10).mapToObj(i -> i + "")
+                                .parallel()
+                                .filter((String i) -> Thread.currentThread()
+                                                                                    .getName()
+                                                                                    .startsWith("testName"))
+                                .count();
+        assertThat(notRenamed,is(0));
+    }
+}
