c17777e9c4369f02ed6b6f8fb48a1af37169d58a
Victor Harbo Johnston <vhol@kb.dk>
2024-01-30 11:20:34 +0100

    implement multiple values visitor

--- src/main/java/dk/kb/util/yaml/YAMLVisitor.java
+++ src/main/java/dk/kb/util/yaml/YAMLVisitor.java
@@ -1,87 +1,15 @@
 package dk.kb.util.yaml;
 
-import org.yaml.snakeyaml.events.Event;
-import org.yaml.snakeyaml.events.MappingEndEvent;
-import org.yaml.snakeyaml.events.MappingStartEvent;
-import org.yaml.snakeyaml.events.ScalarEvent;
-import org.yaml.snakeyaml.events.SequenceEndEvent;
-import org.yaml.snakeyaml.events.SequenceStartEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.util.List;
 
-public class YAMLVisitor {
+public interface YAMLVisitor {
 
-        public static void visitYAML(String yamlPath) throws IOException {
-            YAML yaml = YAML.resolveLayeredConfigs(yamlPath);
-            Iterable<Event> events = yaml.parse(new YamlDocumentReader(yaml.toString()));
+    abstract public void visit(YAML yaml);
 
-            YamlNodeVisitor visitor = new YamlNodeVisitor();
-            for (Event event : events) {
-                event.accept(visitor);
-            }
-        }
-    }
 
-    class YamlNodeVisitor implements EventVisitor {
-
-        @Override
-        public void visit(ScalarEvent event) {
-            System.out.println("Scalar value: " + event.getValue());
-            // Add your logic for scalar nodes
-        }
-
-        @Override
-        public void visit(MappingStartEvent event) {
-            System.out.println("Mapping start");
-            // Add your logic for mapping start
-        }
-
-        @Override
-        public void visit(MappingEndEvent event) {
-            System.out.println("Mapping end");
-            // Add your logic for mapping end
-        }
-
-        @Override
-        public void visit(SequenceStartEvent event) {
-            System.out.println("Sequence start");
-            // Add your logic for sequence start
-        }
-
-        @Override
-        public void visit(SequenceEndEvent event) {
-            System.out.println("Sequence end");
-            // Add your logic for sequence end
-        }
-    }
-
-    class YamlDocumentReader extends Reader {
-        private String content;
-
-        public YamlDocumentReader(String content) {
-            this.content = content;
-        }
-
-        @Override
-        public int read(char[] cbuf, int off, int len) throws IOException {
-            // Implementation to read from content
-            // This is a simplified example; you may need to implement a proper reader
-            // based on your requirements.
-            // Make sure to handle end of content appropriately.
-
-            int length = Math.min(len, content.length());
-            content.getChars(0, length, cbuf, off);
-            content = content.substring(length);
-
-            return length;
-        }
-
-        @Override
-        public void close() throws IOException {
-            // Close resources if needed
-        }
-    }
 
 }
+
+
