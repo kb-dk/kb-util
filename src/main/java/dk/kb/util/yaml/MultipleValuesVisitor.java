88dce079d4154694347b16d4f985a405abc2e01b
Victor Harbo Johnston <vhol@kb.dk>
2024-01-31 09:23:19 +0100

    update getMultipleFromSubYaml to handle individual scalar values

--- src/main/java/dk/kb/util/yaml/MultipleValuesVisitor.java
+++ src/main/java/dk/kb/util/yaml/MultipleValuesVisitor.java
@@ -1,31 +1,60 @@
 package dk.kb.util.yaml;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ *
+ */
 public class MultipleValuesVisitor implements YAMLVisitor{
+    List<String> extractedValues = new ArrayList<>();
 
+    String keyToExtract;
+
+    /**
+     * Visit the input YAML and traverse its structure.
+     * @param yaml
+     */
     @Override
     public void visit(YAML yaml) {
-        traverseYaml(yaml, null, null);
+        traverseYaml(yaml);
     }
 
 
-    public void traverseYaml(Object yamlElement, List<String> extractedValues, String keyToExtract) {
+    /**
+     * @param yamlElement
+     */
+    private void traverseYaml(Object yamlElement) {
         if (yamlElement instanceof Map) {
             Map<?, ?> yamlMap = (Map<?, ?>) yamlElement;
             yamlMap.forEach((key, value) -> {
                 if (key.equals(keyToExtract) && !(value instanceof Map) ){
                     extractedValues.add((String) value);
                 }
-                traverseYaml(value, extractedValues, keyToExtract);
+                traverseYaml(value);
             });
         } else if (yamlElement instanceof Iterable) {
             Iterable<?> yamlList = (Iterable<?>) yamlElement;
             for (Object item : yamlList) {
-                traverseYaml(item, extractedValues, keyToExtract);
+                traverseYaml(item);
             }
         }
     }
 
+    public List<String> getExtractedValues() {
+        return extractedValues;
+    }
+
+    public void setExtractedValues(List<String> extractedValues) {
+        this.extractedValues = extractedValues;
+    }
+
+    public String getKeyToExtract() {
+        return keyToExtract;
+    }
+
+    public void setKeyToExtract(String keyToExtract) {
+        this.keyToExtract = keyToExtract;
+    }
 }
